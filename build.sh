624aed06a4dbb8a7a6183b136e3434e37bb808ea
Erik Göök <erikgook@gmail.com>
2020-07-05 23:57:47 +0200

    Adds webpack

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 rm dev/*
 elm make src/Main.elm --debug --output dev/app.js
+webpack --config webpack.config.js
 cp src/index.html dev/index.html
-cp src/index.js dev/index.js
 cp src/style.css dev/style.css
 cp src/favicon.ico dev/favicon.ico
 
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dev'),
+    filename: 'index.js'
+  }
+};

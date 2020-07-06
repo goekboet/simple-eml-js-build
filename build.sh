8faad119fc9b1fef171b36402999538a039f2469
Erik Göök <erikgook@gmail.com>
2020-07-06 17:29:38 +0200

    Use browserify/babel instead of webpack

--- build.sh
+++ build.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
-rm dev/*
-elm make src/Main.elm --debug --output dev/app.js
-webpack --config webpack.config.js
-cp src/index.html dev/index.html
-cp src/style.css dev/style.css
-cp src/favicon.ico dev/favicon.ico
+build_debug() {
+    echo "Build debug"
 
-ls dev
+    elm make src/Main.elm \
+        --debug \
+        --output dist/app.js
+
+    browserify src/index.js \
+        -o dist/index.js \
+        -t [ babelify --presets [ @babel/preset-env ] ]  
+}
+
+build() {
+    echo "Build prod"
+    elm make src/Main.elm \
+        --optimize \
+        --output dist/app.js 
+
+    uglifyjs dist/app.js -c "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" \
+        | uglifyjs --mangle -o dist/app.js
+
+    browserify src/index.js \
+        -o dist/index.js \
+        -t [ babelify --presets [ @babel/preset-env ] ] 
+
+    uglifyjs dist/index.js \
+        -o dist/index.js
+}
+
+mkdir -p dist
+rm -r dist/*
+
+if [[ $1 = "-debug" ]];
+then build_debug
+else build
+fi
+
+cp src/index.html dist/index.html
+cp src/style.css dist/style.css
+cp src/favicon.ico dist/favicon.ico
+
+ls -lh dist
